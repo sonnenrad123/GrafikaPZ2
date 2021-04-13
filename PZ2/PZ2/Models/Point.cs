a0d80f1b1a36c1bc6af512e066d2f56bd227032c
Ivan Gajic <33852380+sonnenrad123@users.noreply.github.com>
2021-04-13 16:19:39 +0200

    Loading xml and creating entities

---
+++ PZ2/PZ2/Models/Point.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PZ2.Models
+{
+    class Point
+    {
+        public Point()
+        {
+
+        }
+
+        public double X { get; set; }
+
+        public double Y { get; set; }
+    }
+}
